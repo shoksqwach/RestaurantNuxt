a7e68f9ec48341ff36ccbc3f3debd2aa91401004
shoksqwach <megkadet3@gmail.com>
2025-07-07 20:37:23 +0300

    third commit

--- components/DishCard.vue
+++ components/DishCard.vue
@@ -18,18 +18,3 @@ defineProps({
   }
 })
 </script>
-
-const allDishes = [
-  { id: 1, price: '1 499', name: 'Французский луковый суп', weight: '450', category: 'recommendations' },
-  { id: 2, price: '2 499', name: 'Говядина по-бургундски', weight: '450', category: 'recommendations' },
-  { id: 3, price: '2 499', name: 'Киш с грибами и шпинатом', weight: '450', category: 'recommendations' },
-  { id: 4, price: '3 499', name: 'Говядина по-бургундски', weight: '450', category: 'recommendations' },
-  { id: 5, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'hot' },
-  { id: 6, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'hot' },
-  { id: 7, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'hot' },
-  { id: 8, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'hot' },
-  { id: 9, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'salads' },
-  { id: 10, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'salads' },
-  { id: 11, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'salads' },
-  { id: 12, price: '499', name: 'Суп гороховый с конечностями', weight: '450', category: 'salads' },
-]
