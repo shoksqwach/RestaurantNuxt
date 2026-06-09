91820bf76d6d36e4d226097830430dd6d60a96d0
shoksqwach <153737650+shoksqwach@users.noreply.github.com>
2026-06-10 02:29:20 +0300

    prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/RestaurantNuxt/',
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['./assets/css/main.css', './assets/css/font.css'],
